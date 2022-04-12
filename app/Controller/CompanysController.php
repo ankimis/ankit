3f10141cbf6864d85bbb45fc16bb921e937a2f3e
ankit mishra <ankit mishra>
2022-04-12 18:09:16 +0530

    four commit

--- app/Controller/CompanysController.php
+++ app/Controller/CompanysController.php
@@ -44,7 +44,7 @@ class CompanysController extends AppController {
             $this->Company->create();
             $Company = $this->request->data('Company');
             $Company['modelname'] = implode(",",$Company['modelname']);
-            // print_r($Company);exit;
+            print_r($Company);exit;
        
             if ($this->Company->save($Company)) {
                 $this->Session->setFlash(__('Your user has been saved.'));
